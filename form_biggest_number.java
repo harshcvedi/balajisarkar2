d9f1a7070b52240e669dae300554fdc0046b19b6
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2022-11-26 12:27:01 +0530

    Create form_biggest_number.java

---
+++ form_biggest_number.java
@@ -0,0 +1,35 @@
+public static void main(String args[]) {
+        Scanner sc =new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t>0){
+          
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<arr.length;i++){
+                arr[i]=sc.nextInt();
+            }
+            for(int i=0;i<arr.length-1;i++){
+                for(int j=i+1;j<arr.length;j++){
+                    String s=""+arr[i]+arr[j];
+                     String s1=""+arr[j]+arr[i];
+                   Long x=Long.parseLong(s);
+                   Long y=Long.parseLong(s1);
+                   
+                    if(x<y){
+                        int temp=arr[i];
+                        arr[i]=arr[j];
+                        arr[j]=temp;
+
+                }
+            }
+           
+
+            
+        }   for(int i=0;i<arr.length;i++){
+                System.out.print(arr[i]);
+            }
+            System.out.println();
+            t--;
+
+    }
+}
