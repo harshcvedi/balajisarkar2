68b9a69ff6e7846905ccadd7a68107ae417da931
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2022-12-12 16:08:37 +0530

    Create remove_element_leetcode.java

---
+++ remove_element_leetcode.java
@@ -0,0 +1,19 @@
+public int removeElement(int[] nums, int val) {
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            if(nums[end]==val){
+                end--;
+            }else{
+                if(nums[start]==val){
+                    int temp=nums[start];
+                    nums[start]=nums[end];
+                    nums[end]=temp;
+                    start++;
+                    end--;
+                }else{
+                    start++;
+                }
+            }
+        }return end+1;
+    }
