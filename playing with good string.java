40196eb4b8ccfcb4897c9ecd2750cf9e5d7491f2
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2023-02-18 14:50:48 +0530

    Update playing with good string.java

--- playing with good string.java
+++ playing with good string.java
@@ -16,3 +16,9 @@
 		ans = Math.max(ans, count);
 		return ans;
 	}
+public static boolean isvowels(char ch) {
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+			return true;
+		}
+		return false;
+	}
